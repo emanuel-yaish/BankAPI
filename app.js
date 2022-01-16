af66aca06364ff4b1da03eab8b2c08dd6c745872
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-16 10:21:01 +0200

    remove comments and logs

--- app.js
+++ app.js
@@ -9,30 +9,4 @@ app.use(express.json());
 app.use("/", bankRouter);
 app.use("/accounts", accountRouter);
 
-// app.get("/", function (req, res) {
-//   res.status(200).send("Welcome to Bank API");
-// });
-
-// app.get("/accounts", function (req, res) {
-//   try {
-//     res.status(201).send(getAccounts());
-//   } catch (e) {
-//     res.status(400).send({
-//       status: "Failed",
-//       error: e.message,
-//     });
-//   }
-// });
-
-// app.put("/accounts", function (req, res) {
-//   try {
-//     res.status(201).send(handleUpdate(req.body));
-//   } catch (e) {
-//     res.status(400).send({
-//       status: "Failed",
-//       error: e.message,
-//     });
-//   }
-// });
-
 module.exports = app;
